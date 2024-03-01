1796788fbdda42e4f4bc3d042d1f83993931b24b
renanklein <renankleinshow@gmail.com>
2024-03-01 09:40:47 -0300

    Adds neo-tree

--- init.lua
+++ init.lua
@@ -23,7 +23,18 @@ local plugins = {
     'nvim-telescope/telescope.nvim', tag = '0.1.5',
     dependencies = { 'nvim-lua/plenary.nvim' },
   },
-  {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"}
+  {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", 
+      "MunifTanjim/nui.nvim",
+  }
+}
+
+
 }
 local opts = {}
 
@@ -38,6 +49,7 @@ vim.cmd.colorscheme "catppuccin"
 local builtin = require("telescope.builtin")
 vim.keymap.set('n', '<C-p>', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>e', ':Neotree filesystem reveal left<CR>', {})
 
 -- Treesitter config --
 
