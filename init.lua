2605b688e73a500d1d964e66a8982d4a148710ab
renanklein <renankleinshow@gmail.com>
2024-03-04 09:44:11 -0300

    Restructure plugins

--- init.lua
+++ init.lua
@@ -17,39 +17,14 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-local plugins = {
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-  {
-    'nvim-telescope/telescope.nvim', tag = '0.1.5',
-    dependencies = { 'nvim-lua/plenary.nvim' },
-  },
-  {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons", 
-      "MunifTanjim/nui.nvim",
-  }
-}
-
-
-}
 local opts = {}
 
 -- Lazy.nvim config --
-require("lazy").setup(plugins, opts)
+require("lazy").setup("plugins")
 
 -- Catppuccin config --
 require("catppuccin").setup()
 vim.cmd.colorscheme "catppuccin"
-
--- Telescope config --
-local builtin = require("telescope.builtin")
-vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>e', ':Neotree filesystem reveal left<CR>', {})
 
 -- Treesitter config --
 
