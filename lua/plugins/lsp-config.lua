d756b304959c10aed14c11a2eaf7c362004e83b8
renanklein <renankleinshow@gmail.com>
2024-03-07 09:45:47 -0300

    Adds code snippets and debug adapters

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -5,7 +5,7 @@ return {
         require("mason").setup()
       end
     },
-    {      
+    {
      "williamboman/mason-lspconfig.nvim",
       config = function()
         require("mason-lspconfig").setup({
@@ -17,7 +17,12 @@ return {
       "neovim/nvim-lspconfig",
       config = function()
         local lspconfig = require("lspconfig")
-        lspconfig.lua_ls.setup{}
+        local capabilities = require("cmp_nvim_lsp").default_capabilities()
+        
+        lspconfig.lua_ls.setup({
+          capabilities = capabilities
+        })
+
         vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
         vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
         vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, {})
