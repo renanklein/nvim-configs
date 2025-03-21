c1c9a1b9945de76ba413c6d9831d9da520ca6223
renanklein <renankleinshow@gmail.com>
2025-03-21 09:02:59 -0300

    Adds copilot config

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -5,4 +5,4 @@ vim.cmd("set shiftwidth=2")
 vim.cmd("set relativenumber")
 vim.cmd("set clipboard+=unnamedplus")
 vim.g.mapleader = " "
-vim.env.JAVA_HOME="<JAVA_VERSION>"
+vim.env.JAVA_HOME="/home/renanklein/.asdf/installs/java/corretto-23.0.2.7.1"
