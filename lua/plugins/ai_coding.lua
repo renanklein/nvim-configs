e1d9849cb20a1475f7b2a7794b356a6e725641c9
Renan Klein <renan.klein@bemobi.com.br>
2025-04-14 09:54:11 -0300

    Changes ai coding lua file

--- lua/plugins/ai_coding.lua
+++ lua/plugins/ai_coding.lua
@@ -1,65 +1,63 @@
 -- filepath: /home/renanklein/projects/nvim-configs/lua/plugins/ai_coding.lua
 return {
-  -- GitHub Copilot
-  {
-    "github/copilot.vim",
-    event = "InsertEnter",
-    config = function()
-      -- GitHub Copilot configuration
-      vim.g.copilot_no_tab_map = true
-      vim.api.nvim_set_keymap("i", "<C-j>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
-      vim.api.nvim_set_keymap("i", "<C-k>", 'copilot#Previous()', { silent = true, expr = true })
-      vim.api.nvim_set_keymap("i", "<C-l>", 'copilot#Next()', { silent = true, expr = true })
-      vim.api.nvim_set_keymap("i", "<C-o>", 'copilot#Dismiss()', { silent = true, expr = true })
-    end,
-  },
-  
-  -- Avante.nvim for advanced AI coding assistance
-  {
-    "avante/nvim",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-telescope/telescope.nvim",
-      "MunifTanjim/nui.nvim",
-    },
-    config = function()
-      require("avante").setup({
-        -- Configure for GitHub Copilot
-        provider = "github-copilot",
-        
-        -- Configure Claude 3.7 Sonnet as the model
-        model = "claude-3-7-sonnet-20240229",
-        
-        -- Additional configuration
-        keymaps = {
-          toggle = "<leader>ai", -- Toggle Avante
-          execute = "<leader>ae", -- Execute AI command
-          complete = "<leader>ac", -- Request completion
-          explain = "<leader>ax",  -- Explain code
-        },
-        
-        -- UI customization
-        ui = {
-          border = "rounded",
-          width = 0.8,   -- 80% of screen width
-          height = 0.7,  -- 70% of screen height
-          position = "center",
-        },
-        
-        -- Context settings
-        context = {
-          lines_before = 30,  -- Include 30 lines before cursor
-          lines_after = 30,   -- Include 30 lines after cursor
-          include_diagnostics = true,
-        },
-      })
-      
-      -- Add custom keymaps
-      vim.keymap.set("n", "<leader>ag", ":AvanteGenerate<CR>", { noremap = true, silent = true })
-      vim.keymap.set("n", "<leader>af", ":AvanteFixCode<CR>", { noremap = true, silent = true })
-      vim.keymap.set("n", "<leader>ad", ":AvanteCodeDoc<CR>", { noremap = true, silent = true })
-      vim.keymap.set("v", "<leader>ar", ":AvanteRefactor<CR>", { noremap = true, silent = true })
-    end,
-  },
+	{
+		"yetone/avante.nvim",
+		event = "VeryLazy",
+		version = false, -- Never set this value to "*"! Never!
+		opts = {
+			-- add any opts here
+			-- for example
+			provider = "copilot",
+			copilot = {
+				model = "claude-3.7-sonnet",
+				endpoint = "https://api.githubcopilot.com",
+				allow_insecure = false,
+				timeout = 10 * 60 * 1000,
+				temperature = 0,
+				max_completion_tokens = 1000000,
+				reasoning_effort = "high",
+			},
+		},
+		-- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+		build = "make",
+		-- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter",
+			"stevearc/dressing.nvim",
+			"nvim-lua/plenary.nvim",
+			"MunifTanjim/nui.nvim",
+			--- The below dependencies are optional,
+			"echasnovski/mini.pick", -- for file_selector provider mini.pick
+			"nvim-telescope/telescope.nvim", -- for file_selector provider telescope
+			"hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
+			"ibhagwan/fzf-lua", -- for file_selector provider fzf
+			"nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+			"zbirenbaum/copilot.lua", -- for providers='copilot'
+			{
+				-- support for image pasting
+				"HakonHarnes/img-clip.nvim",
+				event = "VeryLazy",
+				opts = {
+					-- recommended settings
+					default = {
+						embed_image_as_base64 = false,
+						prompt_for_file_name = false,
+						drag_and_drop = {
+							insert_mode = true,
+						},
+						-- required for Windows users
+						use_absolute_path = true,
+					},
+				},
+			},
+			{
+				-- Make sure to set this up properly if you have lazy=true
+				"MeanderingProgrammer/render-markdown.nvim",
+				opts = {
+					file_types = { "markdown", "Avante" },
+				},
+				ft = { "markdown", "Avante" },
+			},
+		},
+	}, -- Avante.nvim for advanced AI coding assistance
 }
-
